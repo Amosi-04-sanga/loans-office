d70af7368034b1f3fcd336e1d7fabe6c33625edb
amosi ally sanga <sangaamosi04@gmail.com>
2022-04-29 06:32:05 +0300

    working on react compoments

--- pages/login.jsx
+++ pages/login.jsx
@@ -23,12 +23,12 @@ const Login = () => {
           <h2 className='my-4' >LOGIN TO OFFICE</h2>
           <div className="my-4">
             <label className='block w-full mb-2' htmlFor="email">Enter your email</label>
-            <div className="flex justify-center items-center">
+            <div className="h-10 flex justify-center items-center">
               <svg className='w-10 h-10 p-2 bg-white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z" /></svg>
               <input
                 type="email"
                 id='email'
-                className={`${styles.input} p-2 block w-full border-none outline-none`}
+                className={`${styles.input} text-sm md:text-base px-2 block h-full w-full border-none outline-none`}
                 name='email'
                 value={formData.email}
                 onChange={e => setFormData({ ...formData, email: e.target.value })}
@@ -37,12 +37,12 @@ const Login = () => {
           </div>
           <div className="my-8">
             <label className='block w-full mb-2' htmlFor="password">Enter your password</label>
-            <div className="flex justity-center items-center">
+            <div className="flex h-10 justity-center items-center">
               <svg className='w-10 h-10 p-2 bg-white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z" /></svg>
               <input
                 type="password"
                 id='password'
-                className={`${styles.input} block input w-full p-2 border-none outline-none`}
+                className={`${styles.input} block input h-full w-full px-2 border-none outline-none`}
                 name='password'
                 value={formData.password}
                 onChange={e => setFormData({ ...formData, password: e.target.value })}
--- pages/office/[customerInfo].jsx
+++ pages/office/[customerInfo].jsx
@@ -2,12 +2,17 @@ import axios from 'axios'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import OfficeNav from '../../components/OfficeNav'
+import { BoxLoading } from 'react-loadingg'
+import styles from '../../styles/customerInfo.module.css'
+import moment from 'moment'
+//import swal from 'swal'
 
 
 const CustomerInfo = () => {
-
+  const router = useRouter()
   const [customer, setCustomer] = useState(null)
-
+  const [isActive, setIsActive] = useState(false)
+  const [amount, setAmount] = useState("")
   const Router = useRouter()
   const { customerInfo } = Router.query
   console.log(customerInfo);
@@ -16,44 +21,172 @@ const CustomerInfo = () => {
 
     const getCustomer = async () => {
       await axios.get(`/api/customers/${customerInfo}`)
-      .then(res => {
-        const { data } = res
-        console.log(data)
-        setCustomer(data)
-      }).catch(error => console.log(error))
+        .then(res => {
+          const { data } = res
+          console.log(data)
+          setCustomer(data)
+        }).catch(error => console.log(error))
     }
 
-   getCustomer()
+    getCustomer()
 
   }, [customerInfo])
 
+  const popOutForm = () => {
+    setIsActive(true)
+  }
 
+  const format3dig = (num) => {
+    return Number(num).toLocaleString()
+  }
+
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const obj = {
+      date: new Date(),
+      amount
+    }
+
+    if (amount) {
+      axios.patch(`/api/customers/refunds/${customerInfo}`, obj)
+        .then(res => {
+          const { data } = res
+          console.log(data)
+          console.log("refund successed!");
+        })
+        .catch(error => console.log(error))
+
+      setAmount("")
+      setIsActive(false)
+
+      router.reload(window.location.pathname)
+
+    } else {
+      alert({
+        title: "error!",
+        text: "please fill amount!"
+      })
+    }
+  }
+
+
+  const debtCalc = () => {
+
+    const arr = customer.refund
+
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+      sum += Number(arr[i].amount)
+    }
+
+    const debt = customer.loan - sum
+
+    return debt
+  }
 
 
 
   return (
     <div>
       < OfficeNav />
-      <div className="w-40 h-40">
-        <p>hello amos</p>
 
-        {
-          customer ? (
-            <div className="mt-10 grid">
-              <div className="my-8 w-40 h-40">
-                <p> {customer.debtorName} </p>
-                <img className='w-full h-full' src={customer.debtorPhoto} alt={customer.debtorName} />
-              </div>
-
-              <div className="my-8 w-40 h-40">
-                <p> {customer.sponsorName} </p>
-                <img className='w-full h-full' src={customer.sponsorPhoto} alt={customer.sponsorName} />
-              </div>
+      <div className={styles.mainSection}>
+        <div className={isActive ? `${styles.formOpen}` : `${styles.formClosed}`}>
+          <form method='post' autoComplete='off' className={`${styles} p-2 bg-orange-300`} onSubmit={handleSubmit}>
+            <div className="row">
+              <label className='block pt-4' >Enter amount:</label>
+              <input
+                type="text"
+                name="amount"
+                value={amount}
+                className="p-1 mt-2 mb-8 w-full"
+                onChange={e => setAmount(e.target.value)}
+              />
             </div>
-          ) : 
-          "loading..."
-        }
+            <input className='block hover:bg-blue-300 transition duration-300 cursor-pointer btn p-2 px-4 bg-blue-200 mx-auto' type="submit" />
+          </form>
+        </div>
+
+        <div className={`${styles.topFlex} pt-8 mb-10 flex justify-between items-center`}>
+          <p className={`${styles.bottomline} inline-block`} >Customer details</p>
+          <button onClick={popOutForm} className={`${styles.refundBtn} mt-4 cursor-pointer block p-2 border-none outline-none`} >REFUND</button>
+        </div>
+
+        <section className={isActive ? `${styles.details}` : ""}>
+          <div className="p-8 lg:p-20">
+
+            {
+              customer ? (
+                <div >
+                  <div className="grid grid-cols-1 md:grid-cols-12 gap-4 place-items-center">
+                    <div className="col-span-5">
+                      <p className='pb-2' > Debtor: {customer.debtorName} </p>
+                      <div className={`${styles.photo}`}>
+                        <img className='w-full h-full' src={customer.debtorPhoto} alt={customer.debtorName} />
+                      </div>
+                    </div>
+
+                    <div className="col-span-7">
+                      <p className={styles.info} > <span>Debtor Name:  </span> {customer.debtorName} </p>
+                      <p className={styles.info} > <span>Debtor contact:  </span> {customer.debtorTell} </p>
+                      <p className={styles.info} > <span>Loan:  </span> { format3dig(customer.loan) } </p>
+                      <p className={styles.info} > <span>Date:  </span> {moment(customer.createdAt).format("DD MMM, YYYY")} </p>
+                      <p className={styles.info} >
+                        <span>Amount left: </span>
+
+                        {
+                          debtCalc() <= "0" ?
+                            (
+                              Router.push("/office/customers"),
+                              axios.delete( `/api/customers/${customerInfo}` )
+                            ) :
+                            format3dig(debtCalc())
+                        }
+
+                      </p>
+                      <p className={styles.info} > <span>Home adress:  </span> {customer.homeAdress} </p>
+                      <p className={styles.info} > <span>Bonds: </span> {customer.bonds.join()} </p>
+                    </div>
+                  </div>
+
+                  <div className="mt-20 grid grid-cols-1 md:grid-cols-12 gap-4 place-items-center">
+                    <div className="col-span-5">
+                      <p className='pb-2' > Debtor: {customer.sponsorName} </p>
+                      <div className={`${styles.photo}`}>
+                        <img className='w-full h-full' src={customer.sponsorPhoto} alt={customer.sponsorName} />
+                      </div>
+                    </div>
+
+                    <div className="col-span-7 ">
+                      <p className={styles.info} > <span>sponsor Name:  </span> {customer.sponsorName} </p>
+                      <p className={styles.info} > <span>sponsor contact:  </span> {customer.sponsorTell} </p>
+                    </div>
+                  </div>
+                </div>
+              ) :
+                (<h1 >
+                  <BoxLoading />
+                </h1>)
+            }
+          </div>
+
+          <div className="p-2 py-6 lg:p-8 mt-8">
+            <ol className='list-decimal md:text-left grid place-items-center' >
+              {
+                customer && (
+                  customer.refund.map((obj, index) => (
+                    <li key={index} className='mb-2' > {format3dig(obj.amount)}/= on {moment(obj.date).format("DD MMM, YYYY")} </li>
+                  ))
+                )
+              }
+            </ol>
+          </div>
+
+        </section>
       </div>
+
     </div>
   )
 }
